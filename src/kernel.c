78b4224715de41214efa4604b66f63445881b14d
Tommac1 <tom.maczkowski@gmail.com>
2018-07-17 20:23:59 +0200

    hello world from all 4 cores

--- src/kernel.c
+++ src/kernel.c
@@ -1,11 +1,35 @@
 #include "uart.h"
+#include "utils.h"
 
-void kernel_main(void)
+static volatile int UART_READY = 0;
+
+void kernel_main(unsigned int procid)
 {
-    uart_init();
-    uart_send_string("Hello, world!\r\n");
+    if (procid == 0) {
+        // only 1st core initializes uart
+        uart_init();
+        UART_READY = 1;
+    }
 
-    while (1) {
-        uart_send(uart_recv());
+    while (UART_READY == 0)
+        ; // wait till uart ready
+
+    // + 1 on delay because 1st core goes into endless loop
+    // while multiplying delay time by 0 (1st core)
+    delay((procid * 10000) + 1);
+
+    uart_send_string("Hello, from processor ");
+    uart_send(procid + '0');
+    uart_send_string("\r\n");
+
+    if (procid == 0) {
+        // 1st core responses on uart, rest are waiting
+        while (1) {
+            uart_send(uart_recv());
+        }
+    }
+    else {
+        while(1)
+            ;
     }
 }
