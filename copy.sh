edd9ebbb57c36e1cc0e3c42e0f246ba751464c88
Tommac1 <tom.maczkowski@gmail.com>
2018-08-07 09:05:34 +0200

    copy script update

--- copy.sh
+++ copy.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-cp ./src/config.txt /media/anon/boot/config.txt
-cp ./kernel7.img /media/anon/boot/kernel7.img
+EXT_1=1
+EXT_2=1
 
-`sync`
+while ((EXT_1 != 0 && EXT_2 != 0)) ; do
+    cp ./src/config.txt /media/anon/boot/config.txt &> /dev/null
+    EXT_1=$?
+    cp ./kernel7.img /media/anon/boot/kernel7.img &> /dev/null
+    EXT_2=$?
+done
+
+sync
+
+echo "COPY FINISHED"
+
